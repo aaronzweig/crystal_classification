e73e03ee81169cb2d7556c9365da4ffc8a329004
mrkidney <azweig@stanford.edu>
2018-02-04 14:16:05 -0800

    seems okay?

--- gcn/train_generative.py
+++ gcn/train_generative.py
@@ -42,81 +42,85 @@ flags.DEFINE_integer('seed', 123, 'TF and numpy seed')
 
 flags.DEFINE_integer('gen_count', 100, 'Number of generated toy graphs for accuracy')
 flags.DEFINE_integer('verbose', 1, 'Print shit')
+flags.DEFINE_integer('test_count', 10, 'as')
 
 tf.set_random_seed(FLAGS.seed)
 
-A_orig, A, X, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(FLAGS.dataset, FLAGS.spectral_cap, FLAGS.seed)
-A_orig_, A_, X_, y_train_, _, _, train_mask_, _, _ = load_data(FLAGS.dataset, FLAGS.spectral_cap, FLAGS.seed + 1)
+densities = np.zeros(FLAGS.test_count)
 
-vertex_count = A.shape[2]
-feature_count = X.shape[2]
-model_func = GraphiteGenModel
+for test_iter in range(FLAGS.test_count):
+    FLAGS.seed += 10
+    A_orig, A, X, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(FLAGS.dataset, FLAGS.spectral_cap, FLAGS.seed)
+    A_orig_, A_, X_, y_train_, _, _, train_mask_, _, _ = load_data(FLAGS.dataset, FLAGS.spectral_cap, FLAGS.seed + 1)
 
-placeholders = {
-    'labels': tf.placeholder(tf.float32, shape = (None, y_train.shape[1])),
-    'labels_mask': tf.placeholder(tf.int32),
-    'adj_norm': tf.placeholder(tf.float32, shape = (None, vertex_count, vertex_count)),
-    'adj_orig': tf.placeholder(tf.float32, shape = (None, vertex_count, vertex_count)),
-    'features': tf.placeholder(tf.float32, shape=(None, vertex_count, feature_count)),
-    'dropout': tf.placeholder_with_default(0., shape=(), name = "drop"),
-    'num_features_nonzero': tf.placeholder(tf.int32, name = "help")
-}
+    vertex_count = A.shape[2]
+    feature_count = X.shape[2]
+    model_func = GraphiteGenModel
 
-model = model_func(placeholders, feature_count, vertex_count, logging=True)
+    placeholders = {
+        'labels': tf.placeholder(tf.float32, shape = (None, y_train.shape[1])),
+        'labels_mask': tf.placeholder(tf.int32),
+        'adj_norm': tf.placeholder(tf.float32, shape = (None, vertex_count, vertex_count)),
+        'adj_orig': tf.placeholder(tf.float32, shape = (None, vertex_count, vertex_count)),
+        'features': tf.placeholder(tf.float32, shape=(None, vertex_count, feature_count)),
+        'dropout': tf.placeholder_with_default(0., shape=(), name = "drop"),
+        'num_features_nonzero': tf.placeholder(tf.int32, name = "help")
+    }
 
-def make_session():
-    os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-    if FLAGS.gpu == -1:
-        sess = tf.Session()
-    else:
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.gpu) # Or whichever device you would like to use
-        gpu_options = tf.GPUOptions(allow_growth=True)
-        sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True))
-    return sess
+    model = model_func(placeholders, feature_count, vertex_count, logging=True)
 
-def evaluate(X, A_norm, A_orig, labels, labels_mask, placeholders, training):
-    feed_dict = construct_feed_dict(X, A_norm, A_orig, labels, labels_mask, placeholders)
-    if training:
-        func = model.opt_op
-        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-    else:
-        func = model.loss
-    outs_val = sess.run([func, model.loss, model.accuracy], feed_dict=feed_dict)
-    return outs_val[1], outs_val[2]
+    def make_session():
+        os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+        if FLAGS.gpu == -1:
+            sess = tf.Session()
+        else:
+            os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.gpu) # Or whichever device you would like to use
+            gpu_options = tf.GPUOptions(allow_growth=True)
+            sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True))
+        return sess
 
-
-sess = make_session()
-sess.run(tf.global_variables_initializer())
-
-for epoch in range(FLAGS.epochs):
-
-    val_loss, val_acc = evaluate(X_, A_, A_orig_, y_train_, train_mask_, placeholders, False)
-    train_loss, train_acc = evaluate(X, A, A_orig, y_train, train_mask, placeholders, True)
-
-    if FLAGS.verbose and (epoch + 1) % 50 == 0:
-        print("Epoch:", '%04d' % (epoch + 1),"train_loss=", "{:.5f}".format(train_loss),
-                                        "val_loss=", "{:.5f}".format(val_loss))
+    def evaluate(X, A_norm, A_orig, labels, labels_mask, placeholders, training):
+        feed_dict = construct_feed_dict(X, A_norm, A_orig, labels, labels_mask, placeholders)
+        if training:
+            func = model.opt_op
+            feed_dict.update({placeholders['dropout']: FLAGS.dropout})
+        else:
+            func = model.loss
+        outs_val = sess.run([func, model.loss, model.accuracy], feed_dict=feed_dict)
+        return outs_val[1], outs_val[2]
 
 
-def plot_graph(A):
-    G = nx.from_numpy_matrix(A)
-    nx.draw(G)
-    plt.show()
-    plt.close()
+    sess = make_session()
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(FLAGS.epochs):
+
+        val_loss, val_acc = evaluate(X_, A_, A_orig_, y_train_, train_mask_, placeholders, False)
+        train_loss, train_acc = evaluate(X, A, A_orig, y_train, train_mask, placeholders, True)
+
+        if FLAGS.verbose and (epoch + 1) % 50 == 0:
+            print("Epoch:", '%04d' % (epoch + 1),"train_loss=", "{:.5f}".format(train_loss),
+                                            "val_loss=", "{:.5f}".format(val_loss))
 
 
-feed_dict = construct_feed_dict(X, A, A_orig, y_train, train_mask, placeholders)
-gens = sess.run(model.sample_fair(FLAGS.gen_count), feed_dict=feed_dict)
-
-# kernels = kernel_scores(gens, test)
-# print(kernels)
-
-print(density_estimate(gens))
-
-# for i in range(5):
-#     A = gens[i]
-#     plot_graph(A)
+    def plot_graph(A):
+        G = nx.from_numpy_matrix(A)
+        nx.draw(G)
+        plt.show()
+        plt.close()
 
 
+    feed_dict = construct_feed_dict(X, A, A_orig, y_train, train_mask, placeholders)
+    gens = sess.run(model.sample_fair(FLAGS.gen_count), feed_dict=feed_dict)
+
+    d = density_estimate(gens)
+    print(d)
+    densities[test_iter] = d
+
+    # for i in range(5):
+    #     A = gens[i]
+    #     plot_graph(A)
+
+print(np.mean(densities))
 
 
